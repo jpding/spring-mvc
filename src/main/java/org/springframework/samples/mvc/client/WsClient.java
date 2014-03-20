c59b406cd4e135ad1ff2fb6e06cb4853b3520c30
jpding <jpding@gmail.com>
2014-03-20 12:29:15 +0800

    拿掉webservice，可以使其运行

--- src/main/java/org/springframework/samples/mvc/client/WsClient.java
+++ src/main/java/org/springframework/samples/mvc/client/WsClient.java
@@ -1,14 +1,12 @@
 package org.springframework.samples.mvc.client;
 
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-import org.springframework.samples.mvc.webservice.HelloWorld;
 
 public class WsClient {
 	public static void main(String [] args){
-		JaxWsProxyFactoryBean bean = new JaxWsProxyFactoryBean();
+		/*JaxWsProxyFactoryBean bean = new JaxWsProxyFactoryBean();
 		bean.setAddress("http://127.0.0.1:8080/spring-mvc-showcase/services/helloworld");
 		bean.setServiceClass(HelloWorld.class);
 		HelloWorld hello = bean.create(HelloWorld.class);
-		System.out.println(hello.sayHi("xxx"));
+		System.out.println(hello.sayHi("xxx"));*/
 	}
 }

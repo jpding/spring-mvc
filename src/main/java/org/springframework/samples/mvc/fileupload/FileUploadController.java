776a46e5ab9cdffb96e31b6b2730d22f357370d1
jpding <jpding@gmail.com>
2014-05-16 23:15:46 +0800

    修改log版本1.2.17

--- src/main/java/org/springframework/samples/mvc/fileupload/FileUploadController.java
+++ src/main/java/org/springframework/samples/mvc/fileupload/FileUploadController.java
@@ -2,6 +2,8 @@ package org.springframework.samples.mvc.fileupload;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mvc.extensions.ajax.AjaxUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,18 +17,23 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping("/fileupload")
 public class FileUploadController {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(FileUploadController.class); 
+	
 	@ModelAttribute
 	public void ajaxAttribute(WebRequest request, Model model) {
+		log.info("================================");
 		model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(request));
 	}
 
 	@RequestMapping(method=RequestMethod.GET)
 	public void fileUploadForm() {
+		log.info("================================");
 	}
 
 	@RequestMapping(method=RequestMethod.POST)
 	public void processUpload(@RequestParam MultipartFile file, Model model) throws IOException {
+		log.info("================================");
 		model.addAttribute("message", "File '" + file.getOriginalFilename() + "' uploaded successfully");
 	}
 	
